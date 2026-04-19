de1b6ee9d04e12514f788a563050bc0c822ea0ad
Ousmane <ousmanegitgit@gmail.com>
2026-04-18 23:33:36 -0400

    interactive login javascript completed

---
+++ bookmark-login.js
@@ -0,0 +1,27 @@
+const passwordInput = document.querySelector(".password_input");
+const emailInput = document.querySelector(".email_input"); 
+const loginBtn =  document.querySelector(".login-btn");
+const errorMsg = document.querySelector(".login-error");
+
+loginBtn.addEventListener("click", loginUser);
+
+function loginUser() { 
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    if(!email || !password) { 
+        errorMsg.innerHTML = `<p class="error-text">Please fill in all fields</p>`
+        return;
+    }
+
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const foundUser = users.find(user => user.email === email && user.password === password);
+
+    if (foundUser) { 
+        errorMsg.innerHTML =  `<p style="color: green;">Login successful!</p>`;
+
+    } else { 
+        errorMsg.innerHTML = `<p class="error-text">Invalid email or password</p>`;
+    }
+}
